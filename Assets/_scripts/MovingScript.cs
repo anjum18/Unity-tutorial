9f41693dabe3dcc9f985fcdd8d88a3b687586262
abailly_acs <antonin.bailly@laposte.net>
2017-03-22 17:54:34 +0100

    first commit

---
+++ Assets/_scripts/MovingScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        transform.Translate(new Vector3(5, 0, 5) * Time.deltaTime);
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("rersdf");
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            
+            gameObject.SetActive(false);
+        }
+    }
+}
